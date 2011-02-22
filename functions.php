0a0c3b1a8ec5c6e7e0f372d2faa9f060815452c4
laras126 <laras126@gmail.com>
2011-02-22 18:08:25 -0500

    deleted non-thematic files

--- functions.php
+++ functions.php
@@ -23,8 +23,8 @@
 
 // Unleash the power of Thematic's dynamic classes
 // 
-// define('THEMATIC_COMPATIBLE_BODY_CLASS', true);
-// define('THEMATIC_COMPATIBLE_POST_CLASS', true);
+//define('THEMATIC_COMPATIBLE_BODY_CLASS', true);
+//define('THEMATIC_COMPATIBLE_POST_CLASS', true);
 
 // Unleash the power of Thematic's comment form
 //
@@ -34,6 +34,77 @@
 //
 // define('THEMATIC_COMPATIBLE_FEEDLINKS', true);
 
+///////////////////////
+// ADDING A TAXONOMY //
+///////////////////////
+
+// We don't use this to it's full extent yet, but we could (will?)
+
+// enabling a taxonomy for Medium
+
+function wpfolio_create_taxonomies() {
+register_taxonomy('medium', 'post', array( 
+	'label' => 'Medium',
+	'hierarchical' => false,  
+	'query_var' => true, 
+	'rewrite' => true,
+	'public' => true,
+	'show_ui' => true,
+	'show_tagcloud' => true,
+	'show_in_nav_menus' => true,));
+} 
+add_action('init', 'wpfolio_create_taxonomies', 0);
+
+
+/////////////////////////////////////
+// ADMIN & THEME OPTIONS INTERFACE //
+/////////////////////////////////////
+
+// customize admin footer text to add wpfolio to links
+function wpfolio_admin_footer() {
+	echo 'Thank you for creating with <a href="http://wordpress.org/" target="_blank">WordPress</a>. | <a href="http://codex.wordpress.org/" target="_blank">Documentation</a> | <a href="http://wordpress.org/support/forum/4" target="_blank">Feedback</a> | <a href="http://wpfolio.visitsteve.com/">Theme by WPFolio</a>';
+} 
+add_filter('admin_footer_text', 'wpfolio_admin_footer');
+
+
+
+// Add WPFolio Wiki site as a Dashboard Feed 
+// Thanks to bavotasan.com: http://bavotasan.com/tutorials/display-rss-feed-with-php/ 
+
+function custom_dashboard_widget() {
+
+	$rss = new DOMDocument();
+	$rss->load('http://wpfolio.visitsteve.com/wiki/feed');
+	$feed = array();
+	foreach ($rss->getElementsByTagName('item') as $node) {
+		$item = array ( 
+			'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+			// 'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+			'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+			'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+			);
+		array_push($feed, $item);
+	}
+	$limit = 5; // change how many posts to display here
+	echo '<ul>'; // wrap in a ul
+	for($x=0;$x<$limit;$x++) {
+		$title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+		$link = $feed[$x]['link'];
+		// $description = $feed[$x]['desc'];
+		$date = date('l F d, Y', strtotime($feed[$x]['date']));
+		echo '<li><p><strong><a href="'.$link.'" title="'.$title.'">'.$title.'</a></strong>';
+		echo ' - '.$date.'</p></li>';
+		// echo '<p>'.$description.'</p>';
+	}
+	echo '</ul>';
+	echo '<p class="textright"><a href="http://wpfolio.visitsteve.com/wiki/category/news" class="button">View all</a></p>'; // link to site
+	
+}
+	
+function add_custom_dashboard_widget() {
+	wp_add_dashboard_widget('custom_dashboard_widget', 'WPFolio News', 'custom_dashboard_widget');
+}
+add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
 
 
 ?>
