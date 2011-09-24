3dfdfdbe0e2f50aa5bc86f976f8e663758125203
laras126 <laras126@gmail.com>
2011-09-24 11:46:23 -0400

    attachment thumbnail size, opacity fix

--- library/wpf/wpf-thumbs.php
+++ library/wpf/wpf-thumbs.php
@@ -12,8 +12,118 @@ if ( function_exists( 'add_theme_support' ) ) {
 // THUMBNAILS //
 ////////////////
 
-// http://www.kingrosales.com/how-to-display-your-posts-first-image-thumbnail-automatically-in-wordpress/ -- (although this link is now dead, and function has been significantly hacked, it's worth a credit.)
 
+// Filter the gallery shortcode defaults
+// http://wordpress.stackexchange.com/questions/4343/how-to-customise-the-output-of-the-wp-image-gallery-shortcode-from-a-plugin
+
+add_filter( 'post_gallery', 'my_post_gallery', 10, 2 );
+function my_post_gallery( $output, $attr) {
+    global $post, $wp_locale;
+
+    static $instance = 0;
+    $instance++;
+
+
+    extract(shortcode_atts(array(
+        'order'      => 'ASC',
+        'orderby'    => 'menu_order ID',
+        'id'         => $post->ID,
+        'itemtag'    => 'dl',
+        'icontag'    => 'dt',
+        'captiontag' => 'dd',
+        'columns'    => 3,
+        'size'       => 'wpf-thumb',
+        'include'    => '',
+        'exclude'    => ''
+    ), $attr));
+
+    $id = intval($id);
+    if ( 'RAND' == $order )
+        $orderby = 'none';
+
+    if ( !empty($include) ) {
+        $include = preg_replace( '/[^0-9,]+/', '', $include );
+        $_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+        $attachments = array();
+        foreach ( $_attachments as $key => $val ) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    } elseif ( !empty($exclude) ) {
+        $exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+        $attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+    } else {
+        $attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+    }
+
+    if ( empty($attachments) )
+        return '';
+
+    if ( is_feed() ) {
+        $output = "\n";
+        foreach ( $attachments as $att_id => $attachment )
+            $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+        return $output;
+    }
+
+    $itemtag = tag_escape($itemtag);
+    $captiontag = tag_escape($captiontag);
+    $columns = intval($columns);
+    $itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+    $float = is_rtl() ? 'right' : 'left';
+
+    $selector = "gallery-{$instance}";
+
+    $output = apply_filters('gallery_style', "
+        <style type='text/css'>
+            #{$selector} {
+                margin: auto;
+            }
+            #{$selector} .gallery-item {
+                float: {$float};
+                margin-top: 10px;
+                text-align: center;
+                width: {$itemwidth}%;           }
+            #{$selector} img {
+                border: 2px solid #cfcfcf;
+            }
+            #{$selector} .gallery-caption {
+                margin-left: 0;
+            }
+        </style>
+        <!-- see gallery_shortcode() in wp-includes/media.php -->
+        <div id='$selector' class='gallery galleryid-{$id}'>");
+
+    $i = 0;
+    foreach ( $attachments as $id => $attachment ) {
+        $link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+        $output .= "<{$itemtag} class='gallery-item'>";
+        $output .= "
+            <{$icontag} class='gallery-icon'>
+                $link
+            </{$icontag}>";
+        if ( $captiontag && trim($attachment->post_excerpt) ) {
+            $output .= "
+                <{$captiontag} class='gallery-caption'>
+                " . wptexturize($attachment->post_excerpt) . "
+                </{$captiontag}>";
+        }
+        $output .= "</{$itemtag}>";
+        if ( $columns > 0 && ++$i % $columns == 0 )
+            $output .= '<br style="clear: both" />';
+    }
+
+    $output .= "
+            <br style='clear: both;' />
+        </div>\n";
+
+    return $output;
+}
+
+
+
+// http://www.kingrosales.com/how-to-display-your-posts-first-image-thumbnail-automatically-in-wordpress/ -- (although this link is now dead, and function has been significantly hacked, it's worth a credit.)
 
 // Get post attachments
 function wpf_get_attachments() {
@@ -51,30 +161,5 @@ function my_post_image_html( $html, $post_id, $post_image_id ) {
 	$html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_post_field( 'post_title', $post_id ) ) . '">' . $html . '</a>';
 	return $html;
 }
-
-
-/*----- GET FEATURED IMAGE (1.7 function)----*/
-// Thumbnail Function - this creates a default featured image if one is not specified
-/*function get_thumb ($post_ID){
-    $thumbargs = array(
-    'post_type' => 'attachment',
-    'numberposts' => 1,
-    'post_status' => null,
-    'post_parent' => $post_ID
-    );
-    $thumb = get_posts($thumbargs);
-    if ($thumb) {
-        return wp_get_attachment_image($thumb[0]->ID);
-    }
-} 
-//add_action('thematic_content', 'check_thumb');
-
-function check_thumb() {		
-	global $post;
-			
-	if( !has_post_thumbnail() ) {		
-		echo get_thumb($post->ID); 
-	}
-}*/
 
 ?>
